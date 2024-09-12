e29d303e73bb581dab70e05e51eba0a1a31940b3
Bluemango1992 <james.nichol@hotmail.co.uk>
2024-09-12 22:51:13 +0100

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/companies_house/',  // Add the repo name here
   plugins: [react()],
-});
+})
