18a0228257c9af09dca9028f0f8043169c56c730
Bluemango1992 <james.nichol@hotmail.co.uk>
2024-09-14 11:22:26 +0100

    analyics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from './App.jsx'
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS here
 import './leafletIcons'; // Import leaflet icon configuratio
 import './input.css'; // Import the input CSS here
+import { Analytics } from '@vercel/analytics/react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
